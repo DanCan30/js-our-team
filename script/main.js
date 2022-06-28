d2535015032b905d3e0f9c9540addfc024146eee
DanCan30 <danielecan3008@gmail.com>
2022-06-28 16:16:14 +0200

    Added comments and a function to add cards

--- script/main.js
+++ script/main.js
@@ -1,6 +1,20 @@
 
+    // Create a variable for DOM elements
 const wrapper = document.getElementById("wrapper");
+let addMember = document.getElementById("add-member");
 
+    // Create an array of images that will be chosen casually when the user will create a new card
+let imgPool = [
+    "new-team-member-01.jpg",
+    "new-team-member-02.jpg",
+    "new-team-member-03.jpg",
+    "new-team-member-04.jpg",
+]
+
+    // Create an empty object that will store the values of the inputs and a random image
+let newMember = {};
+
+    // Array of objects (members)
 let teamMembers = [
     {
         name: "Wayne Barnett",
@@ -34,25 +48,91 @@ let teamMembers = [
     },
 ]
 
-for ( let i = 0; i < teamMembers.length; i++) {
-    let card = document.createElement("div");
-    card.classList.add("card");
+createCard();
 
-    card.innerHTML = `
-    <div class="img-container">
-        <img src="./img./${teamMembers[i].imgSrc}" alt="${teamMembers[i].name}">
-    </div>
-    <div class="info-container">
-        <h4>${teamMembers[i].name}</h4>
-        <span>${teamMembers[i].role}</span>
-    </div>`;
+    // Add onclick function on the submit button
+addMember.addEventListener("click", function() {
+
+        // Take the value of the inputs
+    const userName = document.getElementById("user-full-name").value;
+    const userRole = document.getElementById("user-competences").value;
+    
+        // Store the values in the newMember empty object created before
+    newMember = {
+        name: userName,
+        role: userRole,
+        imgSrc: imgPool[randomNumber(4)],
+    };
+        // Push the new object in the members array
+    teamMembers.push(newMember);
+
+    createNewUser();
+});
 
 
 
 
 
 
-    let memberInfo = `${teamMembers[i].name}, ${teamMembers[i].role}, ${teamMembers[i].imgSrc}`;
-    console.log(memberInfo);
-    wrapper.append(card);
+
+
+//                                          FUNCTIONS
+
+    // Function to create cards for every member of the team
+function createCard() {
+
+    for ( let i = 0; i < teamMembers.length; i++) {
+    
+            // Create a div and give it class "card"
+        let card = document.createElement("div");
+        card.classList.add("card");
+    
+            // Set the innerHTML of the div with the infos of every member
+        card.innerHTML = `
+        <div class="img-container">
+            <img src="./img/${teamMembers[i].imgSrc}" alt="${teamMembers[i].name}">
+        </div>
+        <div class="info-container">
+            <h4>${teamMembers[i].name}</h4>
+            <span>${teamMembers[i].role}</span>
+        </div>`;
+    
+            // Create strings with all infos and print them in console
+        let memberInfo = `${teamMembers[i].name}, ${teamMembers[i].role}, ${teamMembers[i].imgSrc}`;
+        console.log(memberInfo);
+
+            // Add the div created to the DOM
+        wrapper.append(card);
+    
+    };
 }
+
+
+    // Function to create a new user
+function createNewUser() {
+    
+            // Create a div and give it class "card"
+        let card = document.createElement("div");
+        card.classList.add("card");
+    
+            // Set the innerHTML with the infos of the last object of the member's array (the one added by the user)
+        card.innerHTML = `
+        <div class="img-container">
+            <img src="./img./${teamMembers[teamMembers.length - 1].imgSrc}" alt="${teamMembers[teamMembers.length - 1].name}">
+        </div>
+        <div class="info-container">
+            <h4>${teamMembers[teamMembers.length - 1].name}</h4>
+            <span>${teamMembers[teamMembers.length - 1].role}</span>
+        </div>`;
+    
+            // Add the div created to the DOM
+        wrapper.append(card);
+    
+    };
+
+
+        // Function to generate a random number, userd to pick a random image from the imgPool array for each new card created
+    function randomNumber(max) {
+        return Math.floor(Math.random() * max);
+    }
+
